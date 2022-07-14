98a1a26eed96790cea62bb6014734fcb4c517165
John Castillo <john.gnu@gmail.com>
2022-07-14 13:49:43 -0400

    proyecto producto

---
+++ productos/src/productos/Principal.java
@@ -0,0 +1,22 @@
+package productos;
+
+/**
+ *
+ * @author desktop
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Producto p1 = new Producto("papa", 10, 5, 2022, "SI");
+        Producto p2 = new Producto("papa", 10, 5, 2022, "SI");
+        Producto p3 = new Producto("papa", 10, 5, 2022, "SI");
+        Producto p4 = new Producto("papa", 10, 5, 2022, "SI");
+        
+        
+    }
+    
+}
